31c62d5a40f832054e7fa3e21d731c0c8aa06956
Eero Vehmanen <eero.vehmanen@gmail.com>
2023-09-08 23:29:47 +0300

    feat: Clean JSON columns (drafting)
    Refs #861naw8xf

---
+++ prefect_qbi/bigquery_schema.py
@@ -0,0 +1,185 @@
+import json
+
+from google.cloud import bigquery
+
+from .utils import convert_to_snake_case
+
+EXCLUDED_COLUMNS = ["_airbyte_raw_id", "_airbyte_meta"]
+CUSTOM_RENAMINGS = {
+    "_airbyte_extracted_at": "_extracted_at",
+}
+SAMPLE_SIZE = 50
+
+
+def _rename(name):
+    snake_cased = convert_to_snake_case(name)
+    customized = CUSTOM_RENAMINGS.get(snake_cased, snake_cased)
+    return customized
+
+
+def transform_schema(source_schema, client, project_id, source_dataset_id, table_name):
+    schema = []
+    select_list = []
+
+    # First, infer how each field should be mapped to new field(s).
+    # mapping is in format:
+    # [
+    #   (field_1, [...]),
+    #   (field_2, [...]),
+    #   ...
+    # ]
+    mapping = get_schema_mapping(
+        source_schema, client, project_id, source_dataset_id, table_name
+    )
+
+    # Then, get actual schema as simple list of fields and generate SQL for selecting
+    # those fields.
+    for original_field, new_fields in mapping:
+        select_list.append(original_field.name)
+
+        for field_item in new_fields:
+            json_key = field_item["json_key"]
+            field = field_item["field"]
+            schema.append(field)
+
+            if original_field.field_type == "JSON" and json_key:
+                selection = f"JSON_EXTRACT({original_field.name}, '$.{json_key}')"
+                type_conversion_func = (
+                    f"LAX_{field.field_type}"
+                    if field.field_type in ("INT64", "BOOL", "FLOAT64", "STRING")
+                    else None
+                )
+                if type_conversion_func:
+                    selection = f"{type_conversion_func}({selection})"
+                select_list.append(selection)
+
+    select_list_str = ", \n".join(select_list)
+    return schema, select_list_str
+
+
+def get_schema_mapping(
+    source_schema, client, project_id, source_dataset_id, table_name
+):
+    """Return mapping from original fields to new fields
+
+    One field can be mapped to multiple fields.
+    """
+    table_ref = f"{project_id}.{source_dataset_id}.{table_name}"
+    filtered_schema = [
+        field for field in source_schema if field.name not in EXCLUDED_COLUMNS
+    ]
+    # Move "_airbyte*" fields to the end.
+    schema = sorted(
+        filtered_schema, key=lambda field: field.name.startswith("_airbyte")
+    )
+
+    # If there are JSON columns, sample data and infer schema.
+    json_columns = [field.name for field in schema if field.field_type == "JSON"]
+    json_column_schemas = infer_columns_from_json_by_sampling(
+        client, json_columns, table_ref
+    )
+
+    schema_mapping = []
+    for field in schema:
+        field_mapping = map_field(field, json_column_schemas)
+        schema_mapping.append(field_mapping)
+
+    return schema_mapping
+
+
+def transform_field(field):
+    if field.field_type == "RECORD":
+        transformed_subfields = [transform_field(f) for f in field.fields]
+        return bigquery.SchemaField(
+            name=_rename(field.name),
+            field_type=field.field_type,
+            mode=field.mode,
+            fields=transformed_subfields,
+        )
+    else:
+        return bigquery.SchemaField(
+            name=_rename(field.name),
+            field_type=field.field_type,
+            mode=field.mode,
+        )
+
+
+def map_field(field, json_column_schemas):
+    new_field = transform_field(field)
+    new_fields = [
+        {
+            "field": new_field,
+            "json_key": None,
+        }
+    ]
+
+    if field.name in json_column_schemas:
+        for key, data_type in json_column_schemas[field.name].items():
+            raw_name = field.name if key is None else f"{field.name}__{key}"
+            name = _rename(raw_name)
+            new_fields.append(
+                {
+                    "field": bigquery.SchemaField(name, data_type, mode="NULLABLE"),
+                    "json_key": key,
+                }
+            )
+
+    return (field, new_fields)
+
+
+def infer_columns_from_json_by_sampling(client, json_columns, table_ref):
+    if not json_columns:
+        return {}
+    json_columns_str = ", ".join(json_columns)
+    query = f"""
+        SELECT {json_columns_str}
+        FROM `{table_ref}`
+        LIMIT {SAMPLE_SIZE}
+    """
+    query_job = client.query(query)
+    rows = list(query_job)
+
+    # Loop through the sampled rows to infer the schema.
+    json_column_schemas = {}
+    for row in rows:
+        for field_name, field_value in row.items():
+            json_column_schemas[field_name] = analyze_json_value(field_value)
+        # TEMP - TODO
+        break
+
+    return json_column_schemas
+
+
+def analyze_json_value(value):
+    """Return mapping from keys to BigQuery data types"""
+    subcolumns = {}
+
+    try:
+        obj = json.loads(value)
+    except (json.decoder.JSONDecodeError, TypeError) as e:
+        # Just keep the original column.
+        return {}
+
+    if isinstance(obj, dict):
+        for key, val in obj.items():
+            subcolumns[key] = get_bigquery_type(val)
+
+    return subcolumns
+
+
+def get_bigquery_type(value):
+    if isinstance(value, bool):
+        return "BOOL"
+    elif isinstance(value, int):
+        return "INT64"
+    elif isinstance(value, float):
+        return "FLOAT64"
+    elif isinstance(value, str):
+        return "STRING"
+    elif value is None:
+        return "STRING"  # Assuming null values are represented as strings in JSON
+    elif isinstance(value, dict):
+        # Nested JSON
+        return "RECORD"
+    else:
+        return "STRING"  # Default to STRING if type is not recognized
--- prefect_qbi/clean.py
+++ prefect_qbi/clean.py
@@ -1,56 +1,38 @@
-import random
 import re
-import time
+from pprint import pprint
 
-from google.api_core.exceptions import Conflict, NotFound
+from google.api_core.exceptions import NotFound
 from google.cloud import bigquery
 
-
-def get_dataset_location(client, project_id, dataset_id):
-    dataset_ref = f"{project_id}.{dataset_id}"
-    dataset = client.get_dataset(dataset_ref)
-    return dataset.location
+from .bigquery_schema import transform_schema
+from .bigquery_utils import create_dataset_with_location, get_dataset_location
+from .utils import convert_to_snake_case, get_unique_temp_table_name
 
 
-def convert_to_snake_case(text):
-    return re.sub(r"(?<!^)(?=[A-Z])", "_", text).lower()
+def transform_dataset(
+    client,
+    project_id,
+    source_dataset_id,
+    destination_dataset_id,
+    table_prefix,
+):
+    source_location = get_dataset_location(client, project_id, source_dataset_id)
+    create_dataset_with_location(
+        client, project_id, destination_dataset_id, source_location
+    )
+    source_dataset_ref = f"{project_id}.{source_dataset_id}"
+    source_tables = client.list_tables(source_dataset_ref)
 
-
-def transform_schema(schema_fields):
-    transformed_fields = []
-    for field in schema_fields:
-        if field.field_type == "RECORD":
-            transformed_subfields = transform_schema(field.fields)
-            transformed_fields.append(
-                bigquery.SchemaField(
-                    name=convert_to_snake_case(field.name),
-                    field_type=field.field_type,
-                    mode=field.mode,
-                    description=field.description,
-                    fields=transformed_subfields,
-                )
-            )
-        else:
-            transformed_fields.append(
-                bigquery.SchemaField(
-                    name=convert_to_snake_case(field.name),
-                    field_type=field.field_type,
-                    mode=field.mode,
-                    description=field.description,
-                )
-            )
-    return transformed_fields
-
-
-def create_dataset_with_location(client, project_id, dataset_id, location):
-    dataset_ref = f"{project_id}.{dataset_id}"
-    dataset = bigquery.Dataset(dataset_ref)
-    dataset.location = location
-    try:
-        client.create_dataset(dataset)
-    except Conflict as e:
-        if "Already Exists" not in str(e):
-            raise e
+    for table in source_tables:
+        transform_table(
+            client,
+            project_id,
+            source_dataset_id,
+            destination_dataset_id,
+            table.table_id,
+            source_location,
+            table_prefix,
+        )
 
 
 def transform_table(
@@ -66,16 +48,15 @@ def transform_table(
     table_name_final = f"{table_prefix}__{table_name_snake_case}"
     source_table_ref = f"{project_id}.{source_dataset_id}.{table_name}"
     source_table = client.get_table(source_table_ref)
-    schema = source_table.schema
+    source_schema = source_table.schema
 
-    destination_schema = transform_schema(schema)
-
-    # Generate a unique temporary table name with timestamp and random number.
-    timestamp = int(time.time())
-    random_suffix = random.randint(1000, 9999)
-    temp_table_id = f"{table_name_final}__temp_{timestamp}_{random_suffix}"
+    # Infer how schema should be transformed.
+    destination_schema, select_list = transform_schema(
+        source_schema, client, project_id, source_dataset_id, table_name
+    )
 
     # Create the temporary destination table with the transformed schema.
+    temp_table_id = get_unique_temp_table_name(table_name_final)
     destination_temp_table_ref = (
         f"{project_id}.{destination_dataset_id}.{temp_table_id}"
     )
@@ -85,16 +66,15 @@ def transform_table(
     client.create_table(destination_temp_table)
 
     try:
-        # Construct and execute the SQL query to copy data.
+        # Copy data.
         query = f"""
             INSERT INTO `{destination_temp_table_ref}`
-            SELECT *
+            SELECT {select_list}
             FROM `{source_table_ref}`
         """
-
         client.query(query).result()
 
-        # Delete the original destination table if it exists
+        # Delete the original destination table if it exists.
         destination_table_ref = (
             f"{project_id}.{destination_dataset_id}.{table_name_final}"
         )
@@ -103,7 +83,7 @@ def transform_table(
         except NotFound:
             pass
 
-        # Rename the temporary table to the original table's name using SQL
+        # Rename the temporary table to the original table's name.
         rename_query = f"""
             ALTER TABLE `{destination_temp_table_ref}`
             RENAME TO `{table_name_final}`
@@ -120,30 +100,3 @@ def transform_table(
             pass
 
         print(f"Error processing table '{table_name}': {e}")
-
-
-def transform_dataset(
-    client,
-    project_id,
-    source_dataset_id,
-    destination_dataset_id,
-    table_prefix,
-):
-    source_location = get_dataset_location(client, project_id, source_dataset_id)
-    create_dataset_with_location(
-        client, project_id, destination_dataset_id, source_location
-    )
-
-    source_dataset_ref = f"{project_id}.{source_dataset_id}"
-    source_tables = client.list_tables(source_dataset_ref)
-
-    for table in source_tables:
-        transform_table(
-            client,
-            project_id,
-            source_dataset_id,
-            destination_dataset_id,
-            table.table_id,
-            source_location,
-            table_prefix,
-        )
