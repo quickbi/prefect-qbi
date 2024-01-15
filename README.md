ab4d61b6e7732c09aebe023003786e0d3165df76
Roope Ruusuvuori <43811718+rooperuu@users.noreply.github.com>
2024-01-15 11:39:07 +0200

    feat: add Dataform run task

--- README.md
+++ README.md
@@ -4,3 +4,13 @@
 
 - `develop` branch is automatically deployed to staging (Docker image is pushed to Artifact Registry with "staging" tag)
 - `main` branch is automatically deployed to production (Docker image is pushed to Artifact Registry with "production" tag)
+
+## Testing
+
+```sh
+python test_clean_dataset.py "<project>" "<source-dataset>" "<destination-dataset>" "<destination-table-prefix>"
+```
+
+```sh
+python test_run_dataform.py "<project>" "<dataform-repository-location>" "<dataform-repository-name>"
+```
---
+++ prefect_qbi/dataform/__init__.py
@@ -0,0 +1,90 @@
+import logging
+import sys
+import time
+
+from google.cloud import dataform_v1beta1
+from prefect import get_run_logger
+
+
+def get_logger():
+    try:
+        return get_run_logger()
+    except Exception:
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
+def _compile(
+    client: dataform_v1beta1.DataformClient,
+    project: str,
+    location: str,
+    repository: str,
+) -> str:
+    logger = get_logger()
+
+    repository_path = (
+        f"projects/{project}/locations/{location}/repositories/{repository}"
+    )
+
+    compilation_result = client.create_compilation_result(
+        parent=repository_path,
+        compilation_result=dataform_v1beta1.CompilationResult(
+            git_commitish="main",
+            code_compilation_config=dataform_v1beta1.CodeCompilationConfig(
+                default_schema="reporting",
+            ),
+        ),
+    )
+
+    if compilation_result.compilation_errors:
+        for compilation_error in compilation_result.compilation_errors:
+            logger.error("Compilation error: %s", compilation_error.message)
+        raise Exception("Compilation reported errors")
+
+    compilation_result_path = compilation_result.name
+
+    return compilation_result_path.removeprefix(
+        f"{repository_path}/compilationResults/"
+    )
+
+
+def _execute(
+    client: dataform_v1beta1.DataformClient,
+    project: str,
+    location: str,
+    repository: str,
+    compilation_result: str,
+):
+    repository_path = (
+        f"projects/{project}/locations/{location}/repositories/{repository}"
+    )
+    compilation_result_path = (
+        f"{repository_path}/compilationResults/{compilation_result}"
+    )
+
+    workflow_invocation = client.create_workflow_invocation(
+        parent=f"projects/{project}/locations/{location}/repositories/{repository}",
+        workflow_invocation=dataform_v1beta1.WorkflowInvocation(
+            compilation_result=compilation_result_path,
+        ),
+    )
+    while (
+        workflow_invocation.state == dataform_v1beta1.WorkflowInvocation.State.RUNNING
+    ):
+        time.sleep(5)
+        workflow_invocation = client.get_workflow_invocation(
+            name=workflow_invocation.name,
+        )
+    if workflow_invocation.state != dataform_v1beta1.WorkflowInvocation.State.SUCCEEDED:
+        raise Exception("Execution terminated unsuccefully")
+
+
+def run(
+    client: dataform_v1beta1.DataformClient,
+    project: str,
+    location: str,
+    repository: str,
+):
+    compilation_result = _compile(client, project, location, repository)
+    _execute(client, project, location, repository, compilation_result)
